8b94d2c3210fa8f78c56a99a5152f06809291378
akosfi <fiakos1997@gmail.com>
2020-03-18 13:28:44 +0100

    initial project

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+//import socket from './modules/socket';
+//import chat from './modules/chat';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    modules: {
+        //socket,
+        //chat,
+    },
+    strict: true,
+});
