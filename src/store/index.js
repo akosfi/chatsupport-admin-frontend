0c0cd5624dc79c4b5d283816a3df9bf4191a4f81
akosfi <fiakos1997@gmail.com>
2020-03-19 18:17:40 +0100

    handle chat client creation

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,14 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import user from './modules/user';
-//import chat from './modules/chat';
+import chatClient from './modules/chatClient';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
         user,
+        chatClient,
         //chat,
     },
     strict: true,
